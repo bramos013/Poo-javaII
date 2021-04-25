d68148b280b8d191a47c9f8d23d6f1b8b11db52b
bramos013 <brunoramos013@gmail.com>
2021-04-25 20:18:20 -0300

    Classe Estoque() finalizado

--- Estoque.java
+++ Estoque.java
@@ -3,55 +3,49 @@ import java.util.ArrayList;
 
 public class Estoque {
 
-	private List<Eletronico> estoqueDeEletronicos = new ArrayList<>();
-
-//Classe Teste do Estoque    
- 	public void listarEstoque(){
-	for(Eletronico eletro : estoqueDeEletronicos){
-		System.out.println(eletro.toString());
-	}
-}
+	private List<Eletronico> listaDeEletronicos = new ArrayList<>();
 
 //adiciona um novo eletrônico no estoque (não pode haver dois eletrônicos com mesmo código. 
 //Retorna true se o eletrônico foi adicionado. Retorna false se o eletrônico não foi adicionado.    Pronta//
 	public boolean cadastra(Eletronico novoEletro){
-		for(Eletronico eletro : estoqueDeEletronicos){			
+		for(Eletronico eletro : listaDeEletronicos){			
 			if(eletro.getCodigo() == novoEletro.getCodigo()){
 				return false;
 			}
 		}
-		estoqueDeEletronicos.add(novoEletro);
+		listaDeEletronicos.add(novoEletro);
 			return true;					
 	}
 
-//Retorna todos os eletrônicos com o nome indicado, ou null se nenhum eletrônico foi encontrado.   Pronta//
+//Retorna todos os eletrônicos com o nome indicado, ou null se nenhum eletrônico foi encontrado.   Corrigir//
 	public String busca(String nome) {
-		String res = "X";
-			for(Eletronico eletro : estoqueDeEletronicos){
+		String res = "x";
+			for(Eletronico eletro : listaDeEletronicos){
 				if(eletro.getNome().equals(nome))
 					eletro.mostrarNome();
+					return res;
 			}
+			res = null;
+			System.out.println(res);
 		return res;
 	}
 
-//Retorna a descrição de todos os eletrônicos cadastrados, com os respectivos preços. Falta Finalizar//
-	public String buscaTudo() {
-		String estoque = null;
-		for(Eletronico eletro : estoqueDeEletronicos){
-			System.out.println(eletro.toString());
-		}
-			return estoque;
+//Retorna a descrição de todos os eletrônicos cadastrados, com os respectivos preços. Pronta//
+public String buscaTudo() {
+	String estoque = null;
+	for(Eletronico eletro : listaDeEletronicos){
+		System.out.println(eletro.toString());
 	}
+		return estoque;
+}
 
 //Retorna o preço do eletrônico com o codigo indicado, ou -1.0 se nenhum eletrônico foi encontrado. Pronto//
 	public double pesquisaPreco(int codigo) {
 		double preco = -1.0;
-			for(Eletronico eletro : estoqueDeEletronicos){
+			for(Eletronico eletro : listaDeEletronicos){
                 if(eletro.getCodigo() == codigo)
 					preco = eletro.getValorBase();                                            									
-			}
-		System.out.println("Valor do Eletrônico R$ " + preco);
+			}		
 			return preco;
 	}
-	
 }
