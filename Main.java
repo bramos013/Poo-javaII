c4e431ca289222d1458df6227b75c5d05fcec952
bramos013 <brunoramos013@gmail.com>
2021-04-25 20:18:42 -0300

    Instancia Classe ACME

--- Main.java
+++ Main.java
@@ -1,28 +1,12 @@
 public class Main {
 
 	public static void main(String[] args) {
-		
-		Estoque estoque = new Estoque();
-		Eletronico eletroUm = new Computador("Desktop Lenovo", 1, 1000, "Desktop");
-		Eletronico eletroDois = new Computador("Notebook Samgung", 2, 1000, "Notebook");
-		Eletronico eletroTres = new Computador("Servidor Dell", 3, 1000, "Servidor");
-		Eletronico eletroQuatro = new Armazenamento("HD Externo ", 4, 0.5, 1000);
-		Eletronico eletroCinco = new Armazenamento("SSD", 5, 0.5, 240);
-		Eletronico eletroSeis = new Armazenamento("HD", 6, 0.5, 500);
-		Eletronico eletroSete = new Impressora("Impressora HP", 7, 1000, "Jato de Tinta");
-		Eletronico eletroOito = new Impressora("Impressora Epson", 8, 1000, "Termica");
-		Eletronico eletroNove = new Impressora("Impressora Samsung", 9, 1000, "Laser");
+		ACMEEletro session = new ACMEEletro();		
+		session.inicializa();
+
+	
 		//Teste Cadastro
-		estoque.cadastra(eletroUm);
-		estoque.cadastra(eletroDois);
-		estoque.cadastra(eletroTres);
-		estoque.cadastra(eletroQuatro);
-		estoque.cadastra(eletroCinco);
-		estoque.cadastra(eletroSeis);
-		estoque.cadastra(eletroSete);
-		estoque.cadastra(eletroOito);
-		estoque.cadastra(eletroNove);
-/*
+	/*
 		//Teste Busca
 	 	estoque.busca("Computador 2");
 		estoque.busca("Computador 1");
@@ -34,7 +18,7 @@ public class Main {
 		estoque.pesquisaPreco(1);
 		estoque.pesquisaPreco(2);
 */
-
+/*
 		eletroUm.calculaPreco();	
 		eletroDois.calculaPreco();
 		eletroTres.calculaPreco();
@@ -54,8 +38,8 @@ public class Main {
 		System.out.println(eletroSete.geraDescricao());
 		System.out.println(eletroOito.geraDescricao());
 		System.out.println(eletroNove.geraDescricao());
+*/
 
-		
 	}
 
 }
