9b68405d95fd36eb94dd9b2351306f8689e19ac4
bramos013 <brunoramos013@gmail.com>
2021-04-25 23:03:55 -0300

    Corrigido Input Tipo e Tecnologia do cadastro()

--- ACMEEletro.java
+++ ACMEEletro.java
@@ -5,8 +5,8 @@ public class ACMEEletro {
 	public void inicializa() {
 		
 		Eletronico eletroUm = new Computador("Desktop Lenovo", 1, 1000, "Desktop");
-		Eletronico eletroDois = new Computador("Notebook Samgung", 2, 1000, "Notebook");
-		Eletronico eletroTres = new Computador("Servidor Dell", 3, 1000, "Servidor");
+		Eletronico eletroDois = new Computador("Notebook Samsung", 2, 1000, "Notebook");
+		Eletronico eletroTres = new Computador("Desktop Lenovo", 3, 1000, "Servidor");
 		Eletronico eletroQuatro = new Armazenamento("HD Externo ", 4, 0.5, 1000);
 		Eletronico eletroCinco = new Armazenamento("SSD", 5, 0.5, 240);
 		Eletronico eletroSeis = new Armazenamento("HD", 6, 0.5, 500);
@@ -17,6 +17,7 @@ public class ACMEEletro {
 		estoque.cadastra(eletroUm);	estoque.cadastra(eletroDois);	estoque.cadastra(eletroTres); 
 		estoque.cadastra(eletroQuatro);	estoque.cadastra(eletroCinco); estoque.cadastra(eletroSeis); 
 		estoque.cadastra(eletroSete); estoque.cadastra(eletroOito);	 estoque.cadastra(eletroNove);
+	
 		executa();
 	}
 
@@ -55,53 +56,62 @@ public class ACMEEletro {
 							System.out.println("Espaco de Armazenamento: ");
 								int espaco = scan.nextInt();								
 							Eletronico novoArmazenamento = new Armazenamento(nomeArmazenamento, codigoArmazenamento, valorBaseArmazenamento, espaco);
-								estoque.cadastra(novoArmazenamento);
+								if(estoque.cadastra(novoArmazenamento) == false){
+									System.out.println("Eletrônico repetido");
+								}
+
 					break;	
 						case 2:	//Opção de Cadastro Computador
-							System.out.println("Nome do Computador: ");
+							System.out.println("Nome do Computador:");
 								String nomeComputador = scan.nextLine();
-							System.out.println("Código do Produto: ");
+							System.out.println("Código do Produto:");
 								int codigoComputador = scan.nextInt();
-							System.out.println("Valor Base do Produto: ");
+							System.out.println("Valor Base do Produto:");
 								double valorBaseComputador = scan.nextDouble();
-							System.out.println("Tipo de Computador: ");
-								String tipo = scan.nextLine();								
-							Eletronico novoComputador = new Computador(nomeComputador, codigoComputador, valorBaseComputador, tipo);
-								estoque.cadastra(novoComputador);
+							System.out.println("Tipo de Computador:");
+								scan.nextLine();
+								String tipo = scan.nextLine();
+							Eletronico novoComputador = new Computador(nomeComputador,codigoComputador,valorBaseComputador,tipo);
+							if(estoque.cadastra(novoComputador) == false){
+								System.out.println("Eletrônico repetido");
+							}
 					break;
 						case 3: //Opção de Cadastro Impressora
-						System.out.println("Nome da Impressora: ");
+						System.out.println("Nome da Impressora: ");						
 							String nomeImpressora = scan.nextLine();
 						System.out.println("Código do Produto: ");
 							int codigoImpressora = scan.nextInt();
 						System.out.println("Valor Base do Produto: ");
 							double valorBaseImpressora = scan.nextDouble();
 						System.out.println("Tecnologia de impressão: ");
-							String tecnologia = scan.nextLine();								
+							scan.nextLine();							
+							String tecnologia = scan.nextLine();							
 						Eletronico novaImpressora = new Impressora(nomeImpressora, codigoImpressora, valorBaseImpressora, tecnologia);
-							estoque.cadastra(novaImpressora);
+						if(estoque.cadastra(novaImpressora) == false){
+							System.out.println("Eletrônico repetido");
+						}
 					break;		
 						case 4: //Opção Sair
-							continua = false;
-					break;
+							continua = false;					
 					}    				
+					break;
 				case 2: //Opção Para Mostrar todas as informações do Estoque
 							estoque.buscaTudo();
 					break;
 				case 3:	//Opção Para Pesquisa por nome
-						
+						System.out.println("Informe o Nome do Eletrônico");
+							String nomeEletronico = scan.nextLine();
+							estoque.busca(nomeEletronico);
 					break;
-
 				case 4: //Opção Para Calculo de Preco
 						System.out.println("Informe o Código do Produto");
 							int codigoProduto = scan.nextInt();
-							System.out.println("Valor do Eletrônico R$ " + codigoProduto);						
-						//System.out.println("Valor Final = " + precoFinal);
+					//		estoque.calculaPreco();
 					break;
 				case 5:  //Sair
-					System.out.println("Que a força esteja com você jovem Padawan, Até logo!\n ");
+					System.out.println("Até logo!\n ");
 					continua = false;
-				break;					
+				break;	
 				}		
 		}
 	}
